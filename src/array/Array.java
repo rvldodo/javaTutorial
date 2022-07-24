194eb0e711b0bfd21ba099caeef1c7b0492ef3f5
rvldodo <nono.qwerty27@gmail.com>
2022-07-24 05:33:51 +0300

    Initial commit

---
+++ src/array/Array.java
@@ -0,0 +1,9 @@
+package array;
+
+public class Array {
+    public static void main(String[] args) {
+        DVD dvdCollection = new DVD("Thor: Love and Thunder", 2022, "Taika Waititi");
+
+        System.out.println(dvdCollection.toString());
+    }
+}
